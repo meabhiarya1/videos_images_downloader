4f1e3162093420fb89d67318060fff98c705be8c
Abhishek kumar <abhi.arya97161@gmail.com>
2024-07-13 16:27:22 +0530

    functionality : adding multiple input boxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useCallback, useState } from "react";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputStates, setInputStates] = useState([]);
+  const [countInputBox, setCountInputBox] = useState(1);
+
+  const handleSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+    },
+    [inputStates]
+  );
+
+  const handleChange = useCallback((e, index) => {
+    const { value } = e.target;
+
+    setInputStates((prevState) => {
+      // Check if the index exists in the previous state
+      const newState = [...prevState];
+      if (newState[index] !== undefined) {
+        // Update the existing value
+        newState[index] = value;
+      } else {
+        // Create a new entry at the index
+        newState[index] = value;
+      }
+      return newState;
+    });
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex items-center justify-center min-h-screen bg-black">
+      <div className="text-2xl flex flex-col justify-center items-center w-full max-w-5xl p-4 bg-slate-700 rounded-lg shadow-md mx-4">
+        <span className="m-4 font-medium p-2 text-center rounded-lg max-w-48">
+          Paste Link Here
+        </span>
+
+        {/* Form to submit  */}
+        <form className="w-full" onSubmit={handleSubmit}>
+          {[...Array(countInputBox)].map((_, index) => (
+            <div key={index}>
+              <label
+                htmlFor={`link-${index}`}
+                className="relative overflow-hidden rounded-md border pl-3 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600 flex my-4"
+              >
+                <input
+                  type="text"
+                  id={`link-${index}`}
+                  placeholder="Paste link here..."
+                  className="peer h-8 w-full border-none bg-transparent placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm items-center my-3 py-4"
+                  value={inputStates[index]}
+                  onChange={(e) => handleChange(e, index)}
+                />
+                <span className="absolute start-3 top-3 -translate-y-1/2 text-xs text-gray-500 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-3 peer-focus:text-xs">
+                  Paste link here...
+                </span>
+              </label>
+            </div>
+          ))}
+
+          {/* Add more input box button*/}
+          <button
+            type="button"
+            className="w-24 flex rounded border border-indigo-600 bg-indigo-600 px-2 py-3 text-sm font:sm md:font-medium text-slate-300 hover:bg-transparent hover:text-indigo-600 text-center items-center justify-center"
+            onClick={() => setCountInputBox(countInputBox + 1)}
+          >
+            More Link
+          </button>
+
+          {/* Download Button */}
+          <button
+            type="submit"
+            className="w-full inline-block rounded border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-slate-300  hover:bg-transparent hover:text-indigo-600 mt-4 text-center"
+          >
+            Download
+          </button>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
